f6f34f4b39f62dd6f7a797dcd34bfbea37f6ed54
BeginnerBen <benreaby100@gmail.com>
2023-03-06 18:11:01 +1100

    Add files via upload

---
+++ dynamic section/js/app.js
@@ -0,0 +1,11 @@
+
+//A page can't be manipulated safely until the document is "ready
+
+
+	const para = document.createElement("section");
+	para.innerHTML = "This is a paragraph.";
+	document.getElementById("mySection").appendChild(para);
+
+
+	$("#navbar1").append(html);
+	$("#navbar2").append(content);
